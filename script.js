7828305c8511602773351dbaefc5676877554f8b
Rasa <rasa.zukauskaite@gmail.com>
2024-11-26 09:04:49 +0200

    modes functionality

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+//dark or light images
+
+function imageMode(color) {
+    image1.scr = `images/aplication.${color}.svg`;
+    image2.src = `images/post-${color}.svg`;
+    image3.src = `images/design-${color}.svg`;
+}
+
+
+
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 /50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
+}
+
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 /50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 /50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
+
 
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
     }
 }
 
